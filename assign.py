a48b53f17705889bf344054c69de0a79551764f0
JeevanaGatla <jeevanagatla@gmail.com>
2025-04-24 17:15:40 +0530

    assign.py

--- assign.py
+++ assign.py
@@ -1,26 +1,23 @@
+#Assign_1
 sentence = input("Enter a sentence: ")
 p_count = sentence.count('p')
 is_alnum = sentence.isalnum()
 print(f"The letter 'p' appears {p_count} times.")
 print(f"The sentence contains only alphabets and numbers: {is_alnum}")
 
-
-
-
+#Assign_2
 user_input = input("Enter any string: ")
 print(f"isupper(): {user_input.isupper()}")
 print(f"islower(): {user_input.islower()}")
 print(f"isdigit(): {user_input.isdigit()}")
 print(f"isalnum(): {user_input.isalnum()}")
 
-
-
+#assign_3
 text = "Python is easy. Python is powerful"
 modified_text = text.replace("Python", "Coding").upper()
 print("Modified text:", modified_text)
 
-
-
+#Assign_4
 sentence2 = input("Enter a sentence: ")
 clean_sentence = sentence2.strip()
 the_count = clean_sentence.lower().count("the")
@@ -28,7 +25,7 @@ print("Cleaned sentence:", clean_sentence)
 print(f"Count of 'the': {the_count}")
 
 
-
+#Assign_5
 first_name = input("Enter your first name: ")
 middle_name = input("Enter your middle name: ")
 last_name = input("Enter your last name: ")
