413bc3818526ed4adabd85361e789589596cb1e9
JeevanaGatla <Jeevanagatla@gmail.com>
2025-05-23 21:24:20 +0530

    first commit

--- Backend/crud.py
+++ Backend/crud.py
@@ -1,40 +1,55 @@
 from fastapi import FastAPI
-
+import os
+import json
+ 
 app = FastAPI()
-
-users = {}
-
+ 
+ 
+users={}
+ 
+ 
+def load_users():
+    with open("db.json", 'r') as f:
+        data = json.load(f)
+        return data
+   
+def save_user(users):
+    with open("db.json", 'w') as f:
+        json.dump(users, f, indent=4)
+ 
 @app.get("/users")
-def get_users():
+def read_users():
+    users = load_users()
     return users
-
-@app.post("/signup/users")
-def create_user(username, password):
-    users.update({username: password})
-    return users
-
-@app.put("/users")
-def update_user(username, password):
-    if username in users:
-        users.update({username: password})
-        return f"User {username} is updated"
+ 
+@app.post("/signup/{username}/{password}")
+def signup(username,password):
+    # users[username]=password
+    users = load_users()
+    users.append({'username': username, 'password': password })
+    save_user(users)
+    return "sucessful"
+ 
+@app.post("/login/{username}/{password}")
+def login(username,password):
+    if username in users and users[username]==password:
+        return "sucessful"
     else:
-        return "User not found"
-    
-@app.delete("/users")
-def delete_user(username):
+        return "unsuccessful"
+ 
+@app.post("/update/{username}/{password}")
+def update(username,password):
     if username in users:
-        del users[username]
-        return f"User {username} is deleted"
+        users[username]=password
+        return "updated sucessfully"
     else:
-        return "User not found"
-    
-
-@app.post("/login/users")
-def login(username, password):
-    if username in users:
-        if password == users[username]:
-            return "Login Success"
+        return "unsuccessful"
+   
+@app.delete("/delete/{usrname}/{password}")
+def delet(usrname,password):
+    if usrname in users and users[usrname]==password:
+        del users[usrname]
+        return "deleted sucessfully"
     else:
-        return "Login Failed. User not found"
-
+        return "unsuccessful"
+   
